2f8257371ad8b4d6e864c74eb70e07da86cd572d
sendyHalim <sendy_ubm@yahoo.com>
2014-12-15 14:38:39 +0700

    Add ghronometer.go

---
+++ ghronometer.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"./executor"
+	"./timer"
+	"flag"
+	"fmt"
+)
+
+const CLR_G = "\x1b[32;1m"
+const CLR_N = "\x1b[0m"
+
+func main() {
+	flag.Parse()
+	var input []string = flag.Args()
+
+	if len(input) < 1 {
+		return
+	}
+
+	var processName string = input[0]
+	var args []string = input[1:]
+
+	t := &timer.Timer{}
+
+	t.Start()
+	executor.Execute(processName, args...)
+
+	fmt.Printf("\n%selapsed time is %d ms%s\n", CLR_G, t.Counter(), CLR_N)
+}
