067b320ac26fa2db1bd69a74eeec074823aca162
Sendy Halim <sendyHalim93@gmail.com>
2014-12-19 19:45:31 +0700

    Add comment, link to SO answer

--- executor/executor.go
+++ executor/executor.go
@@ -7,6 +7,8 @@ import (
 
 func Execute(processName string, argv ...string) {
 	cmd := exec.Command(processName, argv...)
+	// change the stdin, stdout, and stderr so the cmd will print output in the main process output 
+	// http://stackoverflow.com/questions/8875038/redirect-stdout-pipe-of-child-process-in-golang
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
